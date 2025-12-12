85078c3115059f4e778c18f863d6454240b15e6d
1080heidi <heidideleon415@gmail.com>
2025-12-11 20:09:15 -0800

    adding linkedlist

--- main.cpp
+++ main.cpp
@@ -8,14 +8,13 @@
 
 using namespace std;
 
-string Customer{
+string Customer {
     string name;
     string order;
 };
 
-int randInt(int low, int high) {
-    return rand() % (high - low + 1) + low;
-}
+int randInt(int low, int high) { return low + (rand() % (high - low + 1)); }
+
 
 Customer makeCustomer(const vector<string>& names, const vector<>>& orders) {
     Customer c;
@@ -24,6 +23,21 @@ Customer makeCustomer(const vector<string>& names, const vector<>>& orders) {
     c.order = orders[randInt(0, (int)orders.size() - 1)];
     return c;
 }
+
+struct Node {
+    Customer data;
+    Node* next;
+    Node(const Customer& c) : data(c), next(nullptr) {}
+}
+struct CoffeeQueue {
+    Node* head = nullptr;
+    Node* tail = nullptr;
+    int size = 0;
+    
+
+
+}
+
 
 int main(){
     cout << "Simulation starting...\n";
